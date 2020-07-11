a7f29b306987b2e020d4bd50c2625032d53bfb38
Ibad Shah <iamibadd@gmail.com>
2020-07-11 22:53:30 +0500

    Updates

---
+++ precache-manifest.eb4b425a1957831e3f3803bec1e35a5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fdfdd574481a1bb25a8f348756faa04",
+    "url": "/React-Basics-2/index.html"
+  },
+  {
+    "revision": "757af402bb34967d1e11",
+    "url": "/React-Basics-2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "404c445afe40f6f54ddf",
+    "url": "/React-Basics-2/static/js/2.8c7d77fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Basics-2/static/js/2.8c7d77fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "757af402bb34967d1e11",
+    "url": "/React-Basics-2/static/js/main.5b4fb161.chunk.js"
+  },
+  {
+    "revision": "0b4286004e1a37a59d7b",
+    "url": "/React-Basics-2/static/js/runtime-main.66d75533.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Basics-2/static/media/logo.5d5d9eef.svg"
+  }
+]);
